18b5a7a70af6158c486bf7adf16410669c558dc6
Abidin Durdu <abidindrd@gmail.com>
2024-03-17 23:54:25 +0300

    ..

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	puts("gsh~> ");
+	while (1) {
+        char *cmd = NULL;
+        size_t size = 0;
+        
+        printf("gsh~> ");
+
+		if (getline(&cmd, &size, stdin) == -1) {
+			puts("Error reading input");
+			exit(EXIT_FAILURE);
+		}
+
+		printf("got command %s", cmd);
+
+		free(cmd);
+	}
 
 	return 0;
 }
