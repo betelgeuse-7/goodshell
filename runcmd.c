94281ab7df4b23218579c3d56ac2c0ea919ee33a
Abidin Durdu <abidindrd@gmail.com>
2024-04-05 00:19:05 +0300

    ..

--- runcmd.c
+++ runcmd.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "runcmd.h"
+
+#define PATH_DELIM ":"
 
 void run_cmd(char *cmd) {
-	system(cmd);
+	char *exe_loc = get_exe_loc(cmd);
+}
+
+char *get_exe_loc(char *cmd) {
+	char *path = getenv("PATH");
+    char *token;
+
+    token = strtok(path, PATH_DELIM);
+    while (token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, PATH_DELIM);
+    }
 }
--- runcmd.h
+++ runcmd.h
@@ -1,1 +1,2 @@
 void run_cmd(char *cmd);
+char *get_exe_loc(char *cmd);
